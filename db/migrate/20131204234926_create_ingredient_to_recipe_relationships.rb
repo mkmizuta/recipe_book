96d230c73f3a9e6dee892954548dc082c34d6244
liz rush <liz@lizmrush.com>
2013-12-05 11:27:18 -0800

    fixed and migrated relationship tables

--- db/migrate/20131204234926_create_ingredient_to_recipe_relationships.rb
+++ db/migrate/20131204234926_create_ingredient_to_recipe_relationships.rb
@@ -1,7 +1,8 @@
 class CreateIngredientToRecipeRelationships < ActiveRecord::Migration
   def change
     create_table :ingredient_to_recipe_relationships do |t|
-
+    	t.integer :recipe_id
+    	t.integer :ingredient_id
       t.timestamps
     end
   end
